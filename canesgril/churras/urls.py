47d10ac40e122912b4c16cd9ee9bfc5b4f40b8b5
ThulioFrDev <thuliofrdev@gmail.com>
2025-06-28 11:48:18 -0300

    feat: canesgril2806

--- canesgril/churras/urls.py
+++ canesgril/churras/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import index, churrasco
 urlpatterns = [
     path('', index),
-    path('churrasco', churrasco),
+    path('<int:id>', churrasco, name='churrasco'),
 ]
